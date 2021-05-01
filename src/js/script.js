32d351a75381fd06304f99d38221eeabbe3053db
Sorini92 <artembedlinsky@gmail.com>
2021-05-01 16:23:00 +0300

    end 3 block

---
+++ src/js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $('ul.solutions__tabs').on('click', 'li:not(.solutions__tab_active)', function() {
+        $(this)
+          .addClass('solutions__tab_active').siblings().removeClass('solutions__tab_active')
+          .closest('div.container').find('div.solutions__content').removeClass('solutions__content_active').eq($(this).index()).addClass('solutions__content_active');
+      });
+   
+    $('.solutions__inner').slick({
+        speed: 1200,
+        adaptiveHeight: false,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/solutions/arrow_left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/solutions/arrow_right.png"></button>',
+        responsive: [
+            {
+                breakpoint: 980,
+                    settings: {
+                        dots: true,
+                        arrows: false
+                }
+            }
+        ]
+      });
+    
+    
+
+      
+       
+});
